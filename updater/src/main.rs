ad27e462d5be120b114a33eea9933f8814cd8481
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-02-24 12:39:48 -0600

    add more

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -2,6 +2,7 @@ mod funcs;
 extern crate json;
 extern crate reqwest;
 use reqwest::blocking::*;
+use json::JsonValue::{Array, Object};
 
 const VERSION: f32 = 2.69;
 
@@ -10,14 +11,41 @@ fn main() {
     let resp = builder.get("https://raw.githubusercontent.com/Lord-Giganticus/Kaizo-Mario-3D-World-Installer-Remake/main/version.txt");
     let data = resp.send().unwrap().text().unwrap();
     let ver = data.parse::<f32>().unwrap();
+    update(&builder, get_assets_link(&builder));
     if ver < VERSION {
         println!("Downloading latest installer and updater....");
-
+        
     } else {
         println!("No need to update.");
     }
 }
 
-fn update() {
-    
+fn get_assets_link(builder: &Client) -> String {
+    let link = "https://api.github.com/repos/Lord-Giganticus/Kaizo-Mario-3D-World-Installer-Remake/releases";
+    let data = builder.get(link).send().unwrap().text().unwrap();
+    let parsed = json::parse(&data).unwrap();
+    let array = match parsed {
+        Array(arr) => arr,
+        _ => panic!("Bad Value.")
+    };
+    let values = match array.get(0).unwrap() {
+        Object(obj) => obj,
+        _ => panic!("Bad Value.")
+    };
+    let res = String::from(values.get("assets_url").unwrap().as_str().unwrap());
+    return res;
+}
+
+fn update(builder: &Client, assets_link: String) {
+    let data = builder.get(assets_link).send().unwrap().text().unwrap();
+    let parsed = json::parse(&data).unwrap();
+    let array = match parsed {
+        Array(arr) => arr,
+        _ => panic!("Bad Value.")
+    };
+    let values = match array.get(0).unwrap() {
+        Object(obj) => obj,
+        _ => panic!("Bad Value.")
+    };
+    println!("{:?}", values);
 }
